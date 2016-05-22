6582ac49ef7904a7fa8d76b69153cbd5cc243bc4
alexperez2160 <alexperez2160@gmail.com>
2016-05-22 01:42:17 +0000

    Finished calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -6,32 +6,134 @@
 # answer= gets
 # puts answer 
 
-puts "Welcome to Calculator!"
+LANGUAGE = 'sp'
 
-puts "What's the first number!"
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
-number1= gets.chomp 
 
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
+def prompt(key)
+  message = messages(key, LANGUAGE)   # make sure the "messages" method is declared above this line
+  Kernel.puts("=> #{message}")
+end
+
+def num?(input)
+    integer?(input) || float?(input)
+end
+
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def operation_to_message(operation)
+  word = case operation
+           when '1'
+             'Adding'
+           when '2'
+             'Subtracting'
+           when '3'
+             'Multiplying'
+           when '4'
+             'Dividing'
+         end
+
+  x = "A random line of code"
+
+  word
+end
+
+    
+prompt('welcome')
+
+name=''
+loop do 
+    name=gets.chomp
+    if name.empty?()
+        prompt('valid_name')
+    else 
+        break
+    end 
+end
+
+puts ("Hi #{name}")
+
+loop do 
+number1=''
+loop do 
+prompt('first_number')
+number1= gets.chomp
+    if num?(number1)
+        break
+    else
+        prompt('invalid_number')
+    end
+  end  
+ 
+ number2=''
+
+loop do 
 puts "The number is #{number1}"
-
-puts "What's the second number!"
-
+prompt('second_number')
 number2= gets.chomp 
+    if num?(number2)
+        break
+    else
+        prompt ('invalid_number')
+    end
+end
 
+    
 puts "The number is #{number2}"
 
-puts "What operation would you like to perform? 1) add 2) subtract 3) multiply 4)divide"
+operator_prompt= <<-MSG
+    What operation would you like to perform? 
+    1) add 
+    2) subtract 
+    3) multiply 
+    4)divide
+    MSG
 
-operator= gets.chomp
-if operator=='1'
-    result= number1.to_i+number2.to_i
-    elsif operator=='2'
-    result= number1.to_i-number2.to_i
-    elsif operator=='3'
-    result= number1.to_i * number2.to_i
-    else operator=='4'
-        result=number1.to_f / number2.to_f
-        
+puts (operator_prompt)
+
+operation=''
+loop do 
+operation= gets.chomp
+
+    if %w(1 2 3 4).include?(operation)
+        break 
+    else 
+        prompt("Please enter a valid operator")
+    end 
 end 
 
-puts "The result is #{result}."
+prompt("#{operation_to_message(operation)} the two numbers")
+
+result=case operation
+    when '1'
+        number1.to_i+number2.to_i
+    when '2'
+        number1.to_i-number2.to_i
+    when '3'
+        number1.to_i * number2.to_i
+    when '4'
+        result=number1.to_f / number2.to_f
+  end 
+    
+puts ( "The result is #{result}." )
+
+puts ("Do you want to perform another calculation? (Y to calculate again)")
+
+answer= gets.chomp
+break unless answer.downcase().start_with?('y')
+
+end 
+
+prompt("Thank you for using the calculator ")
---
+++ lesson_2/pseudo.rb
@@ -0,0 +1,71 @@
+#Problem 1
+## psuedo
+#### get the input the numbers from user 
+### sum the two integers 
+### return the result 
+
+## formal pseudo
+# start 
+
+## get number 1
+#set number 1= value entered 
+## get number 2
+## set number 2= value entered 
+## set result = number1 (converted to string)+number2 (converted to string)
+
+##end 
+
+
+# Problem 2
+
+## psuedo code 
+## given an array of strings
+## set a emtpy variable 
+### create a variable that will hold them together
+### then cycle through each string in the array
+## save them into the emtpy variable
+### combine them into a string
+
+#start 
+
+## set combined_phrase= emtpy  
+## set iterator=1 
+
+## while iterator< length of array 
+    ## set combined_phrase=value within string collection at space "iterator"
+    ## if iterator <= length of array 
+    ## go to the next iteration 
+    ## set combined_phrase=value within string collection at space "iterator" combined with the previous iteration
+    ## else
+    ## exit the loop 
+    
+    ## print combined_phrase
+    ## iterator=iterator +1 
+    ## end 
+
+
+## Problem 3 
+## pseudo code
+## create a new empty array
+## iterate through the given array saving each new value into the empty array you just created 
+## the array iterator should be increasing by +2 to only capture every other 
+
+## formal code
+## given an array 
+
+## start  
+##set every_other= emtpy array
+## set iterator=1 
+## set array_iterator=1
+
+## while iterator<length of array 
+## set every_other= value within integer collection at space "iterator"
+## if iterator<=length of array 
+#go to next iteration 
+## set every_other= value within integer collection at space iterator
+## else 
+## print every_other array 
+## iterator= iterator+1
+## array_iterator= array_iterator+2
+
+## end 
