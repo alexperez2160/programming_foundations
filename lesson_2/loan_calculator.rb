7299a7c3493c78188b733f4dcc4fba4e65397039
alexperez2160 <alexperez2160@gmail.com>
2016-05-22 02:21:51 +0000

    revised loan calculator

--- lesson_2/loan_calculator.rb
+++ lesson_2/loan_calculator.rb
@@ -22,21 +22,36 @@ name= gets.chomp
 
 puts "Hello #{name}. Let me help you calculate your monthly loan payment"
 
+loop do 
 puts "What is the APR of the loan?"
 
-apr= gets.chomp.to_f
+apr_years= gets.chomp.to_f
 
-quoted_rate=(apr*0.01)/12
+quoted_rate=(apr_years*0.01)/12
+
+puts "The monthly interest rate is #{quoted_rate}"
 
 puts "What is the loan amount?"
 
 amount= gets.chomp.to_f
+puts "The loan amount is $#{amount}"
 
-puts "What is the loan duration in months?"
+puts "What is the loan duration in years?"
 
 duration=gets.chomp.to_i
+duration= duration*12 
+
+puts "The loan duration is #{duration} months."
 
 payment = (amount*(quoted_rate*((1 + quoted_rate)**duration)))/(((1 + quoted_rate)**duration) - 1)
 
 puts "Your monthly payment will be #{payment}."
 
+puts "Do you have any other loans whose monthly payment you'd like to calculate?"
+
+answer= gets.chomp
+
+   break unless answer.downcase().start_with?('y')
+        
+end 
+"Thank you for using the loan calculator"
