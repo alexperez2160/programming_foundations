426f222b52f1dbe527a280cd2fc89497815d9f06
alexperez2160 <alexperez2160@gmail.com>
2016-05-25 14:53:42 +0000

    Finished bonus for rock paper spock

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -12,13 +12,12 @@ def win?(first, second)
   (first == 'spock' && second == 'spock') ||
   (first == 'scissors' && second == 'lizard') ||
   (first == 'paper' && second == 'spock' ) ||
-  (first == 'spock' && second == 'rock')
+  (first == 'spock' && second == 'rock') ||
+  (first == 'lizard' && second == 'paper')
 end
 
-def display_results(player, computer)
-    if win?(player, computer)
-      prompt("You won!")
-    elsif (player == 'rock' && computer == 'paper') ||
+def lose?(player, computer)
+      (player == 'rock' && computer == 'paper') ||
       (player == 'paper' && computer == 'scissors') ||
       (player == 'scissors' && computer == 'rock') ||
       (player == 'lizard' && computer == 'rock') ||
@@ -27,18 +26,36 @@ def display_results(player, computer)
       (player == 'spock' && computer == 'paper') ||
       (player == 'rock' && computer == 'spock') ||
       (player == 'paper' && computer == 'lizard')
-      prompt("You lose!")
-    else
-      prompt("It's a tie")
-    end
 end
 
 loop do
-choice = ''
 
-loop do
-prompt("Choose one: #{VALID_CHOICES.join(',')}")
-choice = gets.chomp
+
+player_score = 0
+computer_score = 0
+
+  loop do 
+   choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Type r for rock, s for scissors, p for paper, sp for spock, and l for lizard")
+    choice = gets.chomp
+
+    case choice 
+      when 's'
+        choice = 'scissors'
+      when 'r'
+        choice = 'rock'
+      when 'p'
+        choice = 'paper'
+      when 'sp'
+        choice = 'spock'
+      when 'l' 
+        choice = 'lizard'
+      else 
+        prompt("Please enter a valid abbreviation.")
+    end
+  
 
   if VALID_CHOICES.include?(choice)
     break
@@ -51,7 +68,38 @@ computer_choice = VALID_CHOICES.sample
 
 puts("You chose #{choice}. The computer chose #{computer_choice}.")
 
-display_results(choice, computer_choice)
+  if win?(choice, computer_choice)
+    prompt("You won this round.")
+  elsif lose?(choice, computer_choice)
+    prompt("You lost this round.")
+  else 
+    prompt("It's a tie.")
+  end
+  
+
+  if win?(choice, computer_choice)
+    player_score += 1 
+  else 
+    computer_score += 1 
+  end 
+
+prompt("The current score:")
+prompt("player: #{player_score}")
+prompt("computer: #{computer_score}")
+
+
+  if player_score == 5 && player_score>computer_score
+    prompt("You won")
+    break 
+  elsif computer_score == 5 && computer_score>player_score 
+    prompt("You lost")
+    break
+  elsif computer_score == 5 && player_score == 5
+    prompt("It's a tie")
+    break
+  end 
+  end 
+
 
 prompt("Do you want to play again?")
 answer = gets.chomp
