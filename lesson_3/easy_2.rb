a97119c07ef6079a59add885ff81ba0ecaa90040
alexperez2160 <alexperez2160@gmail.com>
2016-05-26 22:22:47 +0000

    Finished easy 2

---
+++ lesson_3/easy_2.rb
@@ -0,0 +1,37 @@
+# Question 1
+## 1 ages.assoc("Spot")
+# ages["Spot"]
+# ages.key?("Spot")
+
+# Question 2
+# ages.values.inject(:+)
+
+# #Question 3
+# ages.select!{|k,v| v<100} 
+
+#Question 4 
+
+# munsters_description.capitalize!
+# munsters_description.swapcase!
+# munsters_description.downcase!
+# munsters_description.upcase!
+
+# Question 5
+#ages.merge!(additional_ages)
+
+# Question 6
+# ages.values.min
+
+#Question 7
+
+#/Dino/.match(advice)
+
+# Question 8
+
+ # flintstones.index{|x| x.start_with?("Be")}
+ 
+ # Question 9
+ 
+ #flintstones.map{|x| x[0..2]}
+ # Question 10
+ #same
