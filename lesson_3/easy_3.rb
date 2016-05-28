23c9f14654b8ad78d73d158d11cbce759a9ba5a1
alexperez2160 <alexperez2160@gmail.com>
2016-05-28 02:11:25 +0000

    Finished easy 3

---
+++ lesson_3/easy_3.rb
@@ -0,0 +1,22 @@
+# question 1 
+
+# flintstones= %w(Fred, Barney, Wilma, Betty, BamBam, Pebbles)
+
+# # Question 2
+# flintstones.push("Dino")
+
+# # Question 3
+# flinstones.push("Dino").push("Hobby")
+
+# #Question 4
+# advice = "Few things in life are as important as house training your pet dinosaur."
+
+# advice.slice(0,38)
+
+#Question 5
+
+#statement.count('t')
+
+#Question 6
+# title = "Flintstone Family Members"
+# title.center(20)
