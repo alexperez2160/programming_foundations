9ccc5f5b2339410384224825f3311a05bf85a362
alexperez2160 <alexperez2160@gmail.com>
2016-06-04 16:51:34 +0000

    Almost done with tictactoe

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -9,9 +9,10 @@ def prompt(msg)
 end
 
 def display_board(brd)
+  system 'clear'
   puts ""
   puts "     |     |"
-  puts "  #{brd[1]}   |  #{brd[2]}  |#{brd[3]}"
+  puts "  #{brd[1]} |  #{brd[2]}|#{brd[3]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts " #{brd[4]}    |   #{brd[5]}  |#{brd[6]}"
@@ -31,14 +32,14 @@ def initialize_board
 end 
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] = INITIAL_MARKER}
+  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
 end
 
 
 def player_places_piece!(brd)
   square = ''
   loop do 
-  prompt "Choose a square #{empty_squares(brd).join(',')}"
+  prompt "Choose a square #{empty_squares(brd).join(', ')}"
   square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
       prompt "This is not a valid choice"
@@ -46,9 +47,55 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd) == [].empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9]] + # rows
+                  [[2,5,8], [1,4,7] , [3,6,9]] + # cols
+                  [[1,5,9], [3,5,7]]              #diags
+                  
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+       return 'Player'
+     elsif
+     brd[line[0]] == COMPUTER_MARKER &&
+       brd[line[1]] == COMPUTER_MARKER &&
+       brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+  end
+    nil
+end
+
 board= initialize_board
+
+loop do
+  display_board(board)
+  player_places_piece!(board)
+  break if board_full?(board) || someone_won?(board)
+  
+  computer_places_piece!(board)
+  break if board_full?(board) || someone_won?(board)
+end
+
 display_board(board)
 
-player_places_piece!(board)
-puts board.inspect
-display_board(board)
+if someone_won?(board)
+  prompt "#{detect_winner(board)} won!"
+else
+  prompt "It's a tie"
+end
+
