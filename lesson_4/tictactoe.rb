fcba59050b6f7bf665b98dc7bacd3e74a640593c
alexperez2160 <alexperez2160@gmail.com>
2016-06-04 21:23:58 +0000

    Adding updates to tictactoe

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -41,10 +41,15 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def joinor(arr, delimiter=', ', word='or')
+  arr[-1] = "#{word} #{arr.last}" if arr.size > 1 
+  arr.size == 2 ? arr.join(' ') : arr.join(delimiter)
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square #{empty_squares(brd).join(', ')}"
+    prompt "Choose a square #{joinor(empty_squares(brd), ', ')}"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "This is not a valid choice"
@@ -53,7 +58,16 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  square = nil
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd)
+    break if square
+  end
+
+  if !square 
+    square = empty_squares(brd).sample
+  end
+  
   brd[square] = COMPUTER_MARKER
 end
 
@@ -76,29 +90,38 @@ def detect_winner(brd)
   nil
 end
 
+def find_at_risk_square(line, brd)
+  if brd.values_at(*line).count(PLAYER_MARKER) == 2
+    brd.select{|k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
+  else
+    nil
+  end
+end
+
 loop do
   board = initialize_board
-
   loop do
     display_board(board)
     player_places_piece!(board)
+    puts 
     break if board_full?(board) || someone_won?(board)
 
     computer_places_piece!(board)
     break if board_full?(board) || someone_won?(board)
   end
-
   display_board(board)
 
+  game_outcome= detect_winner(board)
+  
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+    prompt "#{game_outcome} won!"
   else
     prompt "It's a tie"
   end
 
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+prompt "Play again? (y or n)"
+answer = gets.chomp
+break unless answer.downcase.start_with?('y')
 end
 
 prompt "Thanks for playing tictactoe"
